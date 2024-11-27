9ad651ea003e32f8f5f9eadb4ee0550cfc2af372
Bruna Rosa <bruninha20_468@hotmail.com>
2024-11-27 11:34:30 -0300

    test: refactor login tests

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,19 +1,12 @@
 describe('Valida cenários de Login', () => {
 
   beforeEach(()=> {
-    cy.viewport(1920,1080)
-    cy.visit('https://playground.cyskills.com.br/login')
-    cy.contains('h2', 'Faça login')
-    .should('be.visible')
+    cy.acessLoginPage()
   })
 
   it('Deve logar com sucesso', () => {
     cy.login('papito@cyskills.com.br', 'showtime')
-
-    // Valida página de Bem vindo
-    cy.get('[data-cy="welcome-title"]')
-      .should('be.visible')
-      .and('have.text', 'Boas vindas ao Cypress Playground')
+    cy.clickButtonLogin()
     
       cy.get('[data-cy="welcome-text"]')
       .should('be.visible')
@@ -22,21 +15,37 @@ describe('Valida cenários de Login', () => {
 
   it('Não deve logar com sucesso ao enviar email incorreto', () => {
     cy.login('papi@cyskills.com.br', 'showtime')
+    cy.clickButtonLogin()
     cy.messageValidateLogin('E-mail ou senha incorretos. Por favor, tente novamente!')
   })
 
   it('Não deve logar com sucesso ao enviar email não cadastrado', () => {
     cy.login('404@cyskills.com.br', 'showtime')
+    cy.clickButtonLogin()
     cy.messageValidateLogin('E-mail ou senha incorretos. Por favor, tente novamente!')
   })
 
   it('Não deve logar com sucesso ao enviar senha incorreta', () => {
     cy.login('papito@cyskills.com.br', '54898954')
+    cy.clickButtonLogin()
     cy.messageValidateLogin('E-mail ou senha incorretos. Por favor, tente novamente!')
   })
 
   it('Não deve logar com sucesso ao enviar senha com menos de 6 caracteres', () => {
     cy.login('papito@cyskills.com.br', 'show')
+    cy.clickButtonLogin()
     cy.messageValidateLogin('A senha precisa ter pelo menos 6 caracteres. Vamos tentar de novo!')
+  })
+  
+  it('Não deve logar com sucesso ao enviar email em branco', () => {
+    cy.login('{backspace}', 'showtime')
+    cy.clickButtonLogin()
+    cy.messageValidateLogin('Parece que você esqueceu de informar seu e-mail.')
+  })
+
+  it('Não deve logar com sucesso ao enviar em branco', () => {
+    cy.login('papito@cyskills.com.br', '{backspace}')
+    cy.clickButtonLogin()
+    cy.messageValidateLogin('Por favor, digite sua senha para continuar.')
   })
 })
