4c8d840c29be087c04ce09df3a822a303deb38dd
Bruna Rosa <bruninha20_468@hotmail.com>
2024-11-27 18:23:14 -0300

    test: add checkbox test

---
+++ cypress/e2e/checkbox.cy.js
@@ -0,0 +1,26 @@
+describe('Checkbox', ()=> {
+    
+  beforeEach(()=> {
+    cy.acessLoginPage()
+
+    cy.login('papito@cyskills.com.br', 'showtime')
+    cy.clickButtonLogin()
+    cy.userLoggedIn()
+
+    cy.accessMenu('/checkbox', 'Checkbox')
+  })
+
+  it('Deve selecionar o campo de checkbox', () =>{
+     cy.get('input[value=1]')
+      .parent()
+      .click()
+    cy.get('label[for="python"]').click()
+  })
+
+  it('Deve selecionar todos os checkboxes', () =>{
+    const langs = ['javascript', 'python', 'rust', 'go', 'typescript']
+    langs.forEach(lang => {
+      cy.get(`label[for="${lang}"]`).click()
+    })
+  }) 
+})
