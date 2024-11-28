12a13063ca3d08a9a765d1e178e523ce6bdf74e5
Bruna Rosa <bruninha20_468@hotmail.com>
2024-11-28 11:58:26 -0300

    test: add select tests

---
+++ cypress/e2e/select.cy.js
@@ -0,0 +1,44 @@
+describe('Select', ()=> {
+    beforeEach(()=> {
+        cy.acessLoginPage()
+    
+        cy.login('papito@cyskills.com.br', 'showtime')
+        cy.clickButtonLogin()
+        cy.userLoggedIn()
+    
+        cy.accessMenu('/select', 'Select')
+    })
+
+    it('Deve selecionar o framework de teste e linguagem usados no Curso Cypress Skills', () =>{
+        //seleciona o framework de teste
+        cy.contains('label', 'Selecione um Framework de Testes')
+          .parent()  
+          .find('select')
+          .select('Cypress')
+
+    //seleciona a linguagem
+        cy.get('input[placeholder^="Linguagens de programação"]')
+        .click()
+
+        cy.contains('.option-item', 'JavaScript').click()
+    })
+
+    it('Deve selecionar o framework de teste e mais de uma linguagem usados no Curso Cypress Skills', () =>{
+
+        const langs = ['JavaScript', 'TypeScript']
+    
+        //seleciona o framework de teste
+        cy.contains('label', 'Selecione um Framework de Testes')
+          .parent()  
+          .find('select')
+          .select('Cypress')
+
+        //seleciona mais de uma linguagem
+        cy.get('input[placeholder^="Linguagens de programação"]')
+        .click()
+
+        langs.forEach(lang => {
+            cy.contains('.option-item', lang).click()
+          })
+    })
+})
