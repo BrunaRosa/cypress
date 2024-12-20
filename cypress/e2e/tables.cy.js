7cfd68daa9167fdb285b772e8b66cc88fadebdb9
Bruna Rosa <bruninha20_468@hotmail.com>
2024-12-19 21:33:27 -0300

    test: add tables test

---
+++ cypress/e2e/tables.cy.js
@@ -0,0 +1,48 @@
+describe("Tables", () => {
+    beforeEach(() => {
+      cy.acessLoginPage();
+  
+      cy.login("papito@cyskills.com.br", "showtime");
+      cy.clickButtonLogin();
+      cy.userLoggedIn();
+  
+      cy.accessMenu("/tables", "Tables");
+    });
+  
+    it("Deve validar a linha do Github", () => {
+        cy.contains('table tbody tr', '1004')
+
+          .should('be.visible')
+          .and('contain', 'Github')
+          .and('contain', 'papitodev')
+          .and('contain', 'Ativo')
+    });
+
+    it("Deve remover uma rede social", () => {
+      const name = 'Facebook'
+      cy.contains('table tbody tr', '1002')
+        .find('.remove-item')
+        .click()
+
+      cy.contains('button', 'Excluir')
+        .click()
+
+        cy.get('table tbody')
+          .should('not.contain', name)
+      });
+
+      it("Deve permanece na table ao desistir de excluir uma rede social", () => {
+        const name = 'Youtube'
+        cy.contains('table tbody tr', '1005')
+          .find('.remove-item')
+          .click()
+  
+        cy.contains('button', 'Cancelar')
+          .click()
+  
+          cy.get('table tbody')
+            .should('contain', name)
+        });
+  
+  });
+  
